3d633a63f669723051589529d92423dee1aeed34
Daniel Matyas Perendi <dmperendi1@sheffield.ac.uk>
2020-04-02 14:56:40 +0200

    Surprise class added

---
+++ Zatacka_own/Surprise.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zatacka_own
+{
+    class Surprise
+    {
+        private String name;
+        private float posX;
+        private float posY;
+        private readonly Action action; //method
+        public Surprise(String n, float x, float y, Action a)
+        {
+            this.name = n;
+            this.posX = x;
+            this.posY = y;
+            this.action = a;
+        }
+    }
+}
